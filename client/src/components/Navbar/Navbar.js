dec373cd00542fbf6bc292641da85afec2ef7c67
sohamdutta <xzoham@gmail.com>
2021-02-27 18:36:36 -0500

    more components created to clean up code and files restructured

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Avatar, AppBar, Typography, Toolbar, Button } from '@material-ui/core'
+import logo from '../../images/logo.png'
+import useStyles from './styles';
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  const classes = useStyles();
+  const user = null
+
+  return (
+    <AppBar className={classes.appBar} position="static">
+      <div className={classes.brandContainer} />
+      <img component={Link} to="/" className={classes.image} src={logo} alt="icon" height="100" />
+      <Toolbar className={classes.toolbar}>
+        {user ? (
+          <div className={classes.profile}>
+            <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+            <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+            <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+          </div>
+        ) : (
+            <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+          )
+        }
+      </Toolbar>
+    </AppBar>
+  )
+}
