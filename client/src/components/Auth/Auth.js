ffe2caba9efd322a8cd35863ec56d7da01f76e3d
sohamdutta <xzoham@gmail.com>
2021-02-27 19:08:19 -0500

    working on authorization input field

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
+import useStyles from './styles'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 
 export default function Auth() {
-  const state = null
+  const classes = useStyles()
+  const userExists = false
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+
+  }
+
   return (
-    <div>
-      AUTH
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{userExists ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {
+              userExists && (
+                <div>
+                  <TextField xs={6} name="firstName" label="firstName" autoFocus handleChange={handleChange}></TextField>
+                  <TextField xs={6} name="lastName" label="lastName" autoFocus handleChange={handleChange}></TextField>
+                </div>
+              )
+            }
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   )
 }
 
---
+++ client/src/components/Auth/input.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core'
+
+export default function input() {
+  <Grid item xs={12} sm={less ? 6 : 12}>
+    <TextField
+      name={name}
+      onChange={handleChange}
+      variant="outlined"
+    ></TextField>
+  </Grid>
+}
+
+
